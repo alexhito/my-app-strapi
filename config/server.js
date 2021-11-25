27f816ef6c7ad646f1bce41736c470ea5c1ac0c8
alexhito <alex.gato.electro@gmail.com>
2021-11-24 20:58:40 -0500

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'dbfef3551b3b013c94eb74262d122523'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2a89a4e9-33c1-4dba-ab98-b697af323860'
+};
